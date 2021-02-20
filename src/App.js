5eb160cbc369a606929abc7bf0d092bf3c8fd0fc
Iogsotot <ajustusa@gmail.com>
2021-02-20 15:28:20 +0300

    feat: add loader

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React, { useEffect } from 'react';
 import TodoList from './Todo/TodoList';
 import Context from './context';
 import AddTodo from './Todo/AddTodo';
+import Loader from './Loader';
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    // { id: 1, completed: false, title: 'Купить сок' },
-    // { id: 2, completed: false, title: 'Потанцевать' },
-    // { id: 3, completed: false, title: 'Поспать' },
-  ]);
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
       .then((response) => response.json())
       .then((todos) => {
         setTodos(todos);
+        setLoading(false);
       });
   }, []);
 
@@ -50,7 +49,8 @@ function App() {
       <div className='wrapper'>
         <h1>Today tasks:</h1>
         <AddTodo onCreate={addTodo} />
-        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>You have no tasks</p>}
+        {loading && <Loader />}
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : loading ? null : <p>You have no tasks</p>}
       </div>
     </Context.Provider>
   );
